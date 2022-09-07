c49f224918cf5b085302ecf9d333abfd9d0a0389
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-07 10:51:40 -0500

    Curso Udemy React Seccion2: 13. funciones, retorno y argumentoss

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import { Funciones } from './typescript/Funciones';
 // import { TiposBasicos } from "./typescript/TiposBasicos";
-
-import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
+// import { ObjetosLiterales } from './typescript/ObjetosLiterales';
 
 const App = () => {
   return (
-    <div className="mt-2">
+    <div className='mt-2'>
       <h1>Introduccion a TS - React</h1>
       <hr />
       {/* <TiposBasicos />  */}
-      <ObjetosLiterales />
+      {/* <ObjetosLiterales /> */}
+      <Funciones />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
