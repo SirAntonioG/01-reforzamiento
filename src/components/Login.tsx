5267d10695a4415344b3691b621ad18fb5414c06
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-07 19:16:43 -0500

    Curso Udemy React-Native Seccion2 : 18. authReducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 
 interface AuthState {
   validando: boolean;
@@ -16,15 +16,42 @@ const initialState: AuthState = {
 
 type AuthAction = { type: 'logout' };
 
-const authReducer = (state: AuthState, action: AuthAction): AuthState => {};
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case 'logout':
+      return {
+        validando: false,
+        token: null,
+        nombre: '',
+        username: '',
+      };
+
+    default:
+      return state;
+  }
+};
 
 export const Login = () => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: 'logout' });
+    }, 1500);
+  }, []);
+
+  if (state.validando) {
+    return (
+      <>
+        <h3>Login</h3>
+        <div className='alert alert-info'>Validando...</div>
+      </>
+    );
+  }
+
   return (
     <>
       <h3>Login</h3>
-      <div className='alert alert-info'>Validando</div>
       <div className='alert alert-danger'>No Autenticado</div>
       <div className='alert alert-success'>Autenticado</div>
       <button className='btn btn-primary'>Login</button>
