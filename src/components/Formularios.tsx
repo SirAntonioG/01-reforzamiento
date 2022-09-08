640bae960fb0189bd0d1ef9fa66f22adae7b190f
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-08 10:40:42 -0500

    Curso Udemy React-Native Seccion2 : 27. custom hook - useForm - <genericos>

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react';
+import { useForm } from '../hooks/useForm';
 
 export const Formularios = () => {
-  const [formulario, setFormulario] = useState({
+  const { formulario, email, password, onChange } = useForm({
     email: 'test@example.com',
     password: '123456',
   });
-
-  const onChange = (value: string, campo: string) => {
-    setFormulario({
-      ...formulario,
-      [campo]: value,
-    });
-  };
 
   return (
     <>
@@ -20,14 +13,14 @@ export const Formularios = () => {
         type='text'
         className='form-control'
         placeholder='Email'
-        value={formulario.email}
+        value={email}
         onChange={({ target }) => onChange(target.value, 'email')}
       />
       <input
         type='text'
         className='form-control mt-2 mb-2'
         placeholder='Password'
-        value={formulario.password}
+        value={password}
         onChange={({ target }) => onChange(target.value, 'password')}
       />
       <code>
