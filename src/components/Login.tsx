533b96bd1788f18f66b267cd931accab0ecf3ff9
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-07 19:37:07 -0500

    Curso Udemy React-Native Seccion2 : 19. login y logout

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -14,7 +14,12 @@ const initialState: AuthState = {
   nombre: '',
 };
 
-type AuthAction = { type: 'logout' };
+type LoginPayLoad = {
+  username: string;
+  nombre: string;
+};
+
+type AuthAction = { type: 'logout' } | { type: 'login'; payload: LoginPayLoad };
 
 const authReducer = (state: AuthState, action: AuthAction): AuthState => {
   switch (action.type) {
@@ -25,7 +30,14 @@ const authReducer = (state: AuthState, action: AuthAction): AuthState => {
         nombre: '',
         username: '',
       };
-
+    case 'login':
+      const { nombre, username } = action.payload;
+      return {
+        validando: false,
+        token: 'ABC123',
+        nombre,
+        username,
+      };
     default:
       return state;
   }
@@ -40,6 +52,19 @@ export const Login = () => {
     }, 1500);
   }, []);
 
+  const login = () => {
+    dispatch({
+      type: 'login',
+      payload: { nombre: 'Antonio', username: 'SirAntonio' },
+    });
+  };
+
+  const logout = () => {
+    dispatch({
+      type: 'logout',
+    });
+  };
+
   if (state.validando) {
     return (
       <>
@@ -52,10 +77,24 @@ export const Login = () => {
   return (
     <>
       <h3>Login</h3>
-      <div className='alert alert-danger'>No Autenticado</div>
-      <div className='alert alert-success'>Autenticado</div>
-      <button className='btn btn-primary'>Login</button>
-      <button className='btn btn-danger'>Logout</button>
+
+      {state.token ? (
+        <div className='alert alert-success'>
+          Autenticado como {state.nombre}
+        </div>
+      ) : (
+        <div className='alert alert-danger'>No Autenticado</div>
+      )}
+
+      {state.token ? (
+        <button className='btn btn-danger' onClick={logout}>
+          Logout
+        </button>
+      ) : (
+        <button className='btn btn-primary' onClick={login}>
+          Login
+        </button>
+      )}
     </>
   );
 };
